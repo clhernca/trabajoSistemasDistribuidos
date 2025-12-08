ab9bb81338537b3a8c5c889439dd6c84e3960e13
clara <clhernca@unirioja.es>
2025-12-08 18:59:38 +0100

    Version final para entregar

---
+++ cliente.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 >nul
+echo ====================================
+echo   CLIENTE DE SUBASTAS
+echo ====================================
+echo.
+java -jar cliente.jar
+pause
---
+++ servidor.bat
@@ -0,0 +1,8 @@
+@echo off
+chcp 65001 >nul
+echo ====================================
+echo   SERVIDOR DE SUBASTAS
+echo ====================================
+echo.
+java -jar servidor.jar
+pause
